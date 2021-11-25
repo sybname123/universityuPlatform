90ea2979064a75dc7594838c4dbb7b63964d69d0
shaoyingbo <shaoyingbo@enmonster.com>
2021-11-25 20:02:51 +0800

    feat: add 模板样式

--- components/newTemplate/newTemplate.vue
+++ components/newTemplate/newTemplate.vue
@@ -1,7 +1,18 @@
 <template>
 	<view class="template">
 		<view class="template_title">
-			
+			<image src="../../static/logo.png" mode=""></image>
+			<view class="">像少年啦飞驰</view>
+			<view class="">icon</view>
+		</view>
+		<view class="">
+			<view class="posts_text"> 在某个时候我有一个朋友，号称铁牛，铁牛的特征是看上去像头铁牛。我们当时学一篇课文，说到长江有一个急弯的地方有一个小镇，那里就有两座镇江的铁牛时，大家和铁牛相视而笑。当时铁牛就很豪迈，举手说，报告老师，我以后要去支援长江的建设。那时正开家长会，大家纷纷向铁牛的爹恭喜说国家有希望了。铁牛以后就有了一个习惯，就是上课中无论什么时候，在国家需要他的时候就会挺身而出支援建设。在小学四年级的时候，我和铁牛双双留级，理由是考试的时候铁牛看我的试卷。偷看是没有错的，错就错在铁牛偷看的是我的，但是我因为没有及格留级了，所以铁牛也付出了代价。</view>
+			<view class="">
+				<image src="../../static/logo.png" mode=""></image>
+				<image src="../../static/logo.png" mode=""></image>
+				<image src="../../static/logo.png" mode=""></image>
+				<image src="../../static/logo.png" mode=""></image>
+			</view>
 		</view>
 	</view>
 </template>
